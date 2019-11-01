f109a4a63f1281e7faada7fdec66b8d0c897b570
DaniWinston25 <DaniWinston25@users.noreply.github.com>
2019-11-01 16:48:32 -0400

    second intital save

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(/* CODE HERE */) {
+function processLength(num1, num2) {
   /* CODE HERE */
 }
 
